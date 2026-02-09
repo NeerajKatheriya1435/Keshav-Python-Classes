4701c05a0095b9cd0499fd1bc6a6643ef539ae74
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-02-09 16:55:27 +0530

    Started python

---
+++ day1.py
@@ -0,0 +1,61 @@
+# print("Hello Keshav")
+
+# a=8
+# b=6
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+
+# print("Keshav kaise ho")
+
+# This is my simple print statements
+# This is single line comment
+
+# print("Abhinav kaise ho")
+# cjdcjfd
+
+# """
+# This is single line comment 
+# hello keshav how are you
+# what are you doing
+# """
+# str1="asdfghj"
+# print(str1)
+
+# print(' Mohan"s brother is not good ')
+# print("Mohan's brother is not good")
+# print("Mohan's brother is\" not good")
+
+# print('Mohan\'s brother is\" not good')
+# print('Mohan\'s brother is\" not good \\" helo')
+
+# print("My name \nis sheela and \nhow \nneeraj are you")
+
+# print("My name \nis sheela and \nhow \nneeraj are you")
+# print("My name            are you")
+# print("My name\t\t\t\tare you")
+
+# print("My name \\\ are you")
+
+# a=8
+# b=34
+
+# abhishekKumar=89
+# abhishekKumar=89
+# abhishekKumar123456=89
+# abhishekKumar123456!@$%^&*()=89
+# abhishekKumar12345_Neeraj=89
+# print(abhishekKumar12345_Neeraj)
+
+age=56
+AGE=78
+
+age=90
+
+print(age)
+print(AGE)
+
+# print(a+b)
+
+
