34ab12c29817cb82d7d7db1071f89118bc1b579f
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-02-11 11:16:25 +0530

    Strings methods

---
+++ day3.py
@@ -0,0 +1,68 @@
+# var1=input("Enter the number: ")
+
+# print("User input is: ",var1)
+
+# Opeartors
+
+# Arithmetic Operators
+a=3
+b=3
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(a**b)
+# print(a/b)
+
+# 7.89 
+# 6.23
+
+# a=7
+# b=8
+# print(a>b)  #F
+# print(a>=b) # F
+# print(a<b) # T
+# print(a<=b) # T
+# print(a==b) # F
+# print(a!=b) # T
+
+a=5
+# a=a+6
+# a+=6
+# print(a)
+# a-=3
+# print(a)
+# a*=3
+# print(a)
+# a/=8
+# print(a)
+
+# print(15>6 and 4>2 and 4<78)
+# print(not(5>6 or 4>2 or 4<78))
+# var1="Suman"
+# print("hello "+var1)
+
+# str1="hello how are " \
+# "you and what " \
+# "about you"
+# print(str1)
+
+# str1="""
+# dsg
+# gdfgf'
+# gfd
+# hello neeraj
+# """
+
+str1="Abhishek"
+# print(str1[0])
+# print(str1[1])
+# print(len(str1))
+# print(str1[0:5])
+# print(str1[::3])
+
+print(str1[-4:])
+# print(str1[0],end="")
+# print(str1[len(str1)-1])
