02773b72abf6b6f979c7fa69629d6b4a36e97004
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-02-13 16:53:15 +0530

    Strings methods

---
+++ day4.py
@@ -0,0 +1,58 @@
+
+# str1="TechVision Technologies"
+
+# print(str1.upper())
+# inp1=input("Enter Your name: ")
+
+# str1="!!!! Naman !!!!"
+# print(str1.strip("!"))
+# print(str1.upper())
+
+# str2="!!!! Hello Guys !!!!!!"
+# print(str2.rstrip("!"))
+# print(str2.lstrip("!"))
+
+# str1="Shivam Kumar"
+
+# print(str1.replace("Kum","Diwakar"))
+
+# str1="Ram-Nam-Sohan-Geeta-Mohan"
+# arr1=str1.split("-")
+# print(arr1[2])
+
+# str1="hello How aRe you"
+# print(str1.capitalize())
+
+# str1 = "--------------------------"
+# print(str1.center(50))
+# str2="Hello Abhinav"
+# print(str2.center(50))
+# print(str1.center(50))
+
+# str1 = "Welcome to the Console"
+# print(str1.endswith("Console"))
+
+# str1 = "He's name is Dan. He is an honest man."
+# print(str1.find("z"))
+
+# str1 = "He's name is Dan. He is an honest man."
+# print(str1.index("is"))
+
+# print(str1.index("z"))
+
+# str1 = "SonuKuma345678"
+# print(str1.isalnum())
+
+# str1 = "hello World"
+# print(str1.islower())
+# str1 = "We wish you a Merry\t Christmas"
+# print(str1.isprintable())
+
+# str2 = "To Kill A Mocking Bird"
+# print(str2.istitle())
+
+# str1 = "PytHon is a Interpreted Language"
+# print(str1.swapcase())
+
+# str1 = "His name is Dan. Dan is an honest man."
+# print(str1.title())
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,1 @@
 
-# str1="asdfghj"
