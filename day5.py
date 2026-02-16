99fe3005538c9aa1f482b5663c72fd191601f0c8
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-02-16 11:58:47 +0100

    if else and match case

---
+++ day5.py
@@ -0,0 +1,92 @@
+
+# age=45
+# if(age>18):
+#     print("You can drive the car")
+
+# else:
+#     print("You are child")
+#     print("hello")
+    
+# print("Universal Line")
+
+# age=55
+# if(age>18):
+#     print("You can drive the car")
+# # else:
+# #     print("hello how are you") 
+#     if(age==35):
+#         print("You are in special age")
+
+#     else:
+#         print("You are not special")
+
+# else:
+#     print("You are child")
+
+# print("Universal Line")
+
+# mark=99
+
+# if(mark<30):
+#     print("Fail")
+
+# elif(mark<50):
+#     print("Grade C")
+
+# elif(mark<70):
+#     print("Grade B")
+
+# elif(mark<90):
+#     print("Grade A")
+
+# else:
+#     print("Pass")
+
+# mark=99
+
+# if(mark>30):
+#     print("Pass")
+
+# if(mark>60):
+#     print("Grade B")
+
+# if(mark>80):
+#     print("Grade A")
+
+# marks = 15
+# if marks >= 90:
+#     print("Grade: A")
+# elif marks >= 75:
+#     print("Grade: B")
+# elif marks >= 60:
+#     print("Grade: C")
+# else:
+#     print("Fail")
+
+value="Neeraj"
+
+match(value):
+    case 1:
+        print("Sunday")
+    case 2:
+        print("Monday")
+    case 3:
+        print("Tuesday")
+    case 4:
+        print("Wednesday")
+    case 15:
+        print("Thursday")
+    case 6:
+        print("Friday")
+    case "Neeraj":
+        print("Saturday")
+    case _:
+        print("Please Input Value Between 1 to 7")
+
+age=12
+
+if(age>18):
+    print("You can drive the car")
+
+else:
+    print("You are child")
