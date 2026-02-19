b054dc3e5e6d48d70f18b63200820efeb696598e
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-02-19 15:57:22 +0530

    loops for loop

---
+++ day6.py
@@ -0,0 +1,37 @@
+# if(5>9):
+#     print("5 is greater than 9")
+# else:
+#     print("5 is not greater than 9")
+# marks=78
+
+# if marks >= 90:
+#     print("Grade: A")
+# elif marks >= 75:
+#     print("Grade: B")
+# elif marks >= 60:
+#     print("Grade: C")
+# else:
+#     print("Fail")
+
+# num1 = int(input("Enter the the input 1: "))
+# num2 = int(input("Enter the the input 2: "))
+
+# operator = input("Enter the the operator: ")
+
+# match operator:
+#     case "+"|"*":
+#         print(num1+num2)
+#     case "-":
+#         print(num1-num2)
+#     case _:
+#         print("Unknown Operator")
+
+# number = 44
+
+# match number:
+#     case _ if number<0: 
+#         print("Invalid Number")
+#     case _ if number %2 == 0:
+#         print("Even number")
+#     case _ if number %2 == 1: 
+#         print("Odd number")
---
+++ day7.py
@@ -0,0 +1,8 @@
+
+# for i in range(0,11,2):
+#     # for j in range(1,11):
+#     #     print(j)
+#     # print(i)
+#     # print("Hello")
+#     print(i)
+
