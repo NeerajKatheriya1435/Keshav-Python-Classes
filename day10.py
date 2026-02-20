e2cf3d2ee9dae5c8846da594a51e3db342af586f
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-02-20 16:57:05 +0530

    functions creation

---
+++ day10.py
@@ -0,0 +1,23 @@
+# for i in range(3): # i=0,1,2
+#     for j in range(6): # j=0,1,2,3,4,5,6
+#         print(j)
+#     print()
+
+# for i in range(6): #i=0
+#     for j in range(3): # j=0,1,2
+#         print(i,end="")
+#     print()
+
+# *
+# **
+# ***
+# ****
+# *****
+
+# for i in range(5): # i=0,1
+#     for k in range(i+1):
+#         print("*",end=" ")
+#     print()
+
+        
+
---
+++ day11.py
@@ -0,0 +1,27 @@
+
+# def addTwoNum():
+#     a=7
+#     b=9
+#     print(a+b)
+
+# def addTwoNum(num1,num2):
+#     print(num1+num2)
+
+
+# addTwoNum(3,4)
+
+# addTwoNum(6,7)
+
+# addTwoNum(3,6)
+
+# def func1():
+#     print("Hello User")
+#     # return "abhinay"
+
+# print(func1())
+
+def multiPlication(a,b):
+    
+    return a*b
+
+multiPlication(5,7)
