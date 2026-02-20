689eaa53b8192f564735bea36e3e9df5d4f56fbd
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-02-20 16:09:59 +0530

    Break and Continue

---
+++ day8.py
@@ -0,0 +1,24 @@
+# for i  in range(4,8,2):
+#     print(i)
+
+# value=input("Enter the @")
+
+# i=0
+
+# while(i<11):
+#     print(i)
+#     i+=2
+
+# number = 1164
+
+# i=1
+# while(i<10):
+#     print(number*i)
+#     i+=1
+
+# i=0
+
+# while(i<21):
+#     if(i%2==1):
+#         print(i)
+#     i+=1
---
+++ day9.py
@@ -0,0 +1,13 @@
+
+# for i in range(11):
+#     for k in range(11):
+#         print(i,end=" ")
+
+# for i in range(11):
+    # if(i==5):
+    #     break
+    # print(i)
+    # if(i%2==1):
+        # continue
+    # print(i)
+    
