057ec56352cd7267b9fac76f8173892842cbb761
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-03-09 16:44:41 +0530

    tuples and strings

---
+++ day18.py
@@ -0,0 +1,28 @@
+
+# l1=[4,5,5,6]
+
+# l1[2]=99
+
+# l1.append(56)
+# print(l1[1])
+
+# std1=(12,7,2026,12,7,56,12)
+
+# std2=(12,7,2026)
+# std1[2]=99
+# std1.append(67)
+
+# list2=list(std1)
+# list2.append(99)
+# std1=tuple(list2)
+
+# std1
+# print(std1[1:3])
+
+# if 125 not in std1:
+#     print("Yes")
+# else:
+#     print("No")
+
+# print(std1.count(12))
+# print(std1.index(7,2))
---
+++ day19.py
@@ -0,0 +1,38 @@
+
+# greet="Good Morning"
+# name="Keshav"
+
+# print("Hello",greet,"How are you",name)
+# print("Hello {} how are you {}".format(greet,name))
+
+# print("Hello {1} how are you {0}".format(name,greet))
+
+# print("Hello {1} how are you {0}".format(name,greet))
+
+# print(f"Hello {greet} and how are you {name}")
+
+# print("Hello %s how are you %s"%(greet,name))
+
+# set1={34,5,6,7,78,8,45,34,23,89,23,89,7,7,7,77,}
+
+# print(set1)
+
+# m1={1,2,4,5,6,7}
+# m2={5,6,7,8,9,8}
+
+# print(m1.union(m2))
+# print(m1.intersection(m2))
+# m1.add(3)
+# m1.remove(43)
+# m1.discard(4)
+# m1.pop()
+# m1.clear()
+
+# m1.update([8,9,45,3,4])
+# print(m1)
+
+m1={1,2,4,5,6,7}
+m2={5,6,7,8,9,8}
+
+# print(m1.difference(m2))
+print(m1.symmetric_difference(m2))
