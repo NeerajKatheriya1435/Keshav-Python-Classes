163661a912c6e31b2c3879bab1cc0b20ba622c43
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-03-11 16:28:53 +0530

    pytho maths library and os

---
+++ day22.py
@@ -0,0 +1,24 @@
+
+
+# list1=[4,7,34,67]
+# for key,item in enumerate(list1,start=10):
+#     print(key,item)
+
+import math
+# import math as m
+# from math import *
+# from math import sqrt as s
+
+# print(math.sqrt(25))
+
+# print(m.sqrt(25))
+# print(m.ceil(2.1))
+# print(m.floor(2.1))
+
+# print(sqrt(45))
+# print(s(45))
+
+# print(sqrt(45))
+# print(ceil(4.75))
+
+# print(dir(math))
---
+++ day23.py
@@ -0,0 +1,25 @@
+import os
+
+# print(os.getcwd())
+# os.mkdir("Abhinay")
+# os.mkdir("Keshav")
+# os.makedirs("Rahan/Suman/John")
+
+
+# os.chdir("./Abhinay")
+# print(os.getcwd())
+# print(os.listdir())
+# os.rename("Keshav","Krshav")
+
+# os.rmdir("Krshav")
+
+# for i in range(101):
+#     os.mkdir(f"Day-{i}")
+# for i in range(101):
+#     os.rename(f"Day-{i}",f"Keshav-{i}")
+
+# for i in range(101):
+#     os.rmdir(f"Keshav-{i}")
+
+# print(os.environ)
+print(os.getenv("PATH"))
