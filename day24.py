6f0b5ae26a4c38e3a3628326976c0aefb7ee07eb
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-03-13 16:46:10 +0530

    os module local global

---
+++ day24.py
@@ -0,0 +1,26 @@
+
+import os
+
+# print(os.getcwd())
+
+# os.mkdir("Abhinay1")
+# os.chdir("./Abhinay")
+# print(os.getcwd())
+
+# os.makedirs("Shivam/Rohan/Nisha")
+# os.makedirs("Shivam/Geeta")
+# print(os.listdir())
+
+# os.remove("delete.txt")
+# os.rename("Rahan","John")
+
+# os.rmdir("John")
+
+# for i in range(101):
+#     os.mkdir(f"MyFile_{i}")
+
+# for i in range(101):
+#     os.rename(f"MyFile_{i}",f"Roahn_{i}")
+
+# for i in range(101):
+#     os.rmdir(f"Roahn_{i}")
---
+++ day25.py
@@ -0,0 +1,13 @@
+# x=12
+
+# print(x)
+x=13
+
+def greet():
+    # global x
+    x=56
+    print(x)
+
+greet()
+
+print(x)
